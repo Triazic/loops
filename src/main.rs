ddc9d0083104708abe4852aa66d1bfb3f69ee9ea
Oliver <oliver.e.v@gmail.com>
2024-07-06 00:08:31 +1000

    Num trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod xy;
+use num_traits::Num;
+
+use xy::XY;
+
 fn main() {
-    println!("Hello, world!");
+    let xy = XY::<f32> { x: 10.0, y: 10.0 };
+    let res = do_something(xy);
+    println!("Hello, world! {}", res);
+}
+
+fn do_something<T: Num>(xy: XY::<T>) -> T {
+    xy.x + xy.y
+}
+
+fn do_another_thing<T: Num>(xy: XY::<T>) -> T {
+    xy.x * xy.y
 }
---
+++ src/xy.rs
@@ -0,0 +1,6 @@
+use num_traits::Num;
+
+pub struct XY<T: Num> {
+    pub x: T,
+    pub y: T,
+}
